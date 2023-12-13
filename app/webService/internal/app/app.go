1c8c04f1e53cdb6887990efaebcfb7b76da0cf21
enchik0reo <profipingvin@gmail.com>
2023-12-13 15:56:39 +0700

    Web Service | Add logs

--- app/webService/internal/app/app.go
+++ app/webService/internal/app/app.go
@@ -1,17 +1,23 @@
 package app
 
 import (
+	"log/slog"
+
 	"newsWebApp/app/webService/internal/config"
+	"newsWebApp/app/webService/internal/logs"
 )
 
 type App struct {
 	cfg *config.Config
+	log *slog.Logger
 }
 
 func New() *App {
 	a := App{}
 
 	a.cfg = config.MustLoad()
+
+	a.log = logs.Setup(a.cfg.Env)
 
 	return &a
 }
